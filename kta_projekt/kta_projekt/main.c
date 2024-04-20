e6f4e55e5f397f7a45848f15905d2807e5bf34a1
Tomáš Martiník <tomas@martinik.eu>
2024-04-21 00:10:51 +0200

    sensor library + simulated temperature works

--- kta_projekt/kta_projekt/main.c
+++ kta_projekt/kta_projekt/main.c
@@ -9,6 +9,17 @@
 #include "modbus.h"
 #include "timer.h"
 #include "UART.h"
+#include "sensor.h"
+
+#define TEMP_REGISTER      0  // Aktuální teplota
+#define UPPER_LIMIT        1  // Horní limit teploty
+#define LOWER_LIMIT        2  // Dolní limit teploty
+#define STATUS_WORD        3  // Status word
+
+#define WATCHDOG_BIT       (1 << 0)
+#define TEMP_VALID_BIT     (1 << 1)
+#define UPPER_LIMIT_BIT    (1 << 2)
+#define LOWER_LIMIT_BIT    (1 << 3)
 
 int main(void) {
 	// Inicializace Modbus komunikace
@@ -16,26 +27,19 @@ int main(void) {
 	modbus_init(9600, 0, 1);  // Nastaví baud rate na 9600, bez parity, 1 stop bit
 
 	// Předpokládejme, že máme holding registry již inicializované
-	holdingRegisters[0] = 1;
-	holdingRegisters[1] = 2;
-	holdingRegisters[2] = 3;
-	holdingRegisters[3] = 4;
+	//holdingRegisters[0] = 1;
+	//holdingRegisters[1] = 2;
+	//holdingRegisters[2] = 3;
+	//holdingRegisters[3] = 4;
 
 	timerInit();
+	initializeHoldingRegisters();
 
-	// Neustálá smyčka, simulující provoz zařízení
 	while(1) {
-		// Simulace příchozí zprávy: Přečti registery od indexu 0, celkem 4 registry
-		//handleReadHoldingRegisters(0, 4);
-		
+	
+		updateSystemState();
 		processIncomingRequests();
-
-		// Simulace chybného příchozího požadavku: Neplatná adresa
-		//handleReadHoldingRegisters(5, 1);  // Tato adresa je mimo rozsah
-
-		// Simulace chybného příchozího požadavku: Neplatný počet registrů
-		//handleReadHoldingRegisters(2, 3);  // Tento požadavek přesahuje maximální počet registrů
-		//printf("ssss\n");
+		
 		delay(1000000);
 
 		
@@ -43,5 +47,3 @@ int main(void) {
 
 	return 0;
 }
-
-
